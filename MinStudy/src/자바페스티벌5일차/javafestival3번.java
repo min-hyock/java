eff0874deb1ce3d4456e6cb73fb12c7ee57e345d
min-hyock <qornzmffjq@naver.com>
2023-02-19 22:47:04 +0900

    23.02.19

--- MinStudy/src/자바페스티벌5일차/javafestival3번.java
+++ MinStudy/src/자바페스티벌5일차/javafestival3번.java
@@ -6,39 +6,22 @@ public class javafestival3번 {
 
 	public static void main(String[] args) {
 
-		
-		int[]array = new int[6];
+		int[] array = new int[6];
 		Random ran = new Random();
-		
-		for(int i = 0;i <array.length;i++) {
-			array[i] = ran.nextInt(45)+1;
-			for(int j = 0;j<i;j++) {
-				if(array[i]==array[j]) {
-					array[i] = ran.nextInt(45)+1;
+
+		for (int i = 0; i < array.length; i++) {
+			array[i] = ran.nextInt(45) + 1;
+			for (int j = 0; j < i; j++) {
+				if (array[i] == array[j]) {
+					array[i] = ran.nextInt(45) + 1;
 					i--;
 				}
 			}
 		}
-		for(int k = 0;k <array.length;k++) {
+		for (int k = 0; k < array.length; k++) {
 			System.out.println("행운의 숫자 : " + array[k]);
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
 
 }
